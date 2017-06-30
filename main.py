73823ebc62661eb370b2cfe7186ad70ef7d8648f
bigblackbug <bigblackbugg@gmail.com>
2017-06-30 04:33:48 +0300

    added a 'expanded_vector_size' argument

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from argparse import ArgumentParser
+
 from math_utils import vector_utils
 
+DEFAULT_NUMBER_OF_PIECES = 2
 if __name__ == '__main__':
-    vector_size = 6
+    parser = ArgumentParser(description='Dot product calculation test client')
+    parser.add_argument("-s", "--expanded_vector_size", type=int, default=6,
+                        help="generated vectors will have the specified size")
+    args = parser.parse_args()
     first_vector = vector_utils.generate_dense_vector(
-        expanded_vector_size=vector_size,
-        number_of_pieces=2)
+        expanded_vector_size=args.expanded_vector_size,
+        number_of_pieces=DEFAULT_NUMBER_OF_PIECES)
     second_vector = vector_utils.generate_dense_vector(
-        expanded_vector_size=vector_size,
-        number_of_pieces=2)
-    print("Calculating dot product of vectors")
+        expanded_vector_size=args.expanded_vector_size,
+        number_of_pieces=DEFAULT_NUMBER_OF_PIECES)
+    print("Calculating dot product of dense vectors")
     print(first_vector)
     print(second_vector)
     dot_product = vector_utils.dot_product(first_vector, second_vector)
-    print(dot_product)
+    print("Dot product: {}".format(dot_product))
